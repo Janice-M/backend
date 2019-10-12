410ea6fa3e7cbd3dffa988b248b448abb2da881e
Janice-M <janiceink001@gmail.com>
2019-10-12 12:53:17 +0300

    set up server to communicate with app.js

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,23 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+    console.log('Rafiki your request has been received !');
+    next();
+});
 
+app.use((req, res, next) => {
+    res.status(201);
+    next();
+});
+
+app.use((req, res, next) => {
+    res.json( 'Asante0, Your request was successful!');
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('Response sent successfully!');
+});
 
 module.exports = app;
